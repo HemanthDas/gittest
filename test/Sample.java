d4d7ee6c60971897be8d1bca55af1276979c30c6
HemanthDas <hemanthchoudary4@gmail.com>
2023-12-28 15:46:45 +0530

    Error contained

--- test/Sample.java
+++ test/Sample.java
@@ -1,6 +1,8 @@
 public class Sample {
     public static void main(String args[]) {
         String str = "i am a new person here";
+        Scanner s = new Scanner(System.in);
+        s.next = 10;
         System.out.println(str+efbe jkf ,k ,jfwe ffss );
         System.out.println(str);
     }
