2d892f0e6f657233b293e88c127f5968414e0a5d
HemanthDas <hemanthchoudary4@gmail.com>
2023-12-28 10:43:45 +0530

    Modified

--- test/Sample.java
+++ test/Sample.java
@@ -2,5 +2,6 @@ public class Sample {
     public static void main(String args[]) {
         String str = "i am a new person here";
         System.out.println(str);
+        System.out.println(str);
     }
 }
