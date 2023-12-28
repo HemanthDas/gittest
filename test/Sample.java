e8cc7d0171c213d20f39aeb1e3215465fbd746a9
HemanthDas <hemanthchoudary4@gmail.com>
2023-12-28 15:41:53 +0530

    Error contained

--- test/Sample.java
+++ test/Sample.java
@@ -1,7 +1,7 @@
-public class SampleTwoi {
+public class Sample {
     public static void main(String args[]) {
         String str = "i am a new person here";
-        System.out.println(str);
+        System.out.println(str+efbe jkf ,k ,jfwe ffss );
         System.out.println(str);
     }
 }
